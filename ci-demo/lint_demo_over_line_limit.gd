39e6b042e63286bec08467d0277772c84b5b0905
stardustsuperwizard <47020213+stardustsuperwizard@users.noreply.github.com>
2026-08-25 17:57:17 -0400

    demo(199): max-file-lines alongside fixable findings, same file and another

---
+++ ci-demo/lint_demo_over_line_limit.gd
@@ -0,0 +1,1369 @@
+extends Node
+
+# Deliberately over gdlint's 1000-line limit. Throwaway CI demonstration
+# file for issue #199; never merged.
+
+
+func _step_0() -> void:
+	pass
+
+
+func _step_1() -> void:
+	pass
+
+
+func _step_2() -> void:
+	pass
+
+
+func _step_3() -> void:
+	pass
+
+
+func _step_4() -> void:
+	pass
+
+
+func _step_5() -> void:
+	pass
+
+
+func _step_6() -> void:
+	pass
+
+
+func _step_7() -> void:
+	pass
+
+
+func _step_8() -> void:
+	pass
+
+
+func _step_9() -> void:
+	pass
+
+
+func _step_10() -> void:
+	pass
+
+
+func _step_11() -> void:
+	pass
+
+
+func _step_12() -> void:
+	pass
+
+
+func _step_13() -> void:
+	pass
+
+
+func _step_14() -> void:
+	pass
+
+
+func _step_15() -> void:
+	pass
+
+
+func _step_16() -> void:
+	pass
+
+
+func _step_17() -> void:
+	pass
+
+
+func _step_18() -> void:
+	pass
+
+
+func _step_19() -> void:
+	pass
+
+
+func _step_20() -> void:
+	pass
+
+
+func _step_21() -> void:
+	pass
+
+
+func _step_22() -> void:
+	pass
+
+
+func _step_23() -> void:
+	pass
+
+
+func _step_24() -> void:
+	pass
+
+
+func _step_25() -> void:
+	pass
+
+
+func _step_26() -> void:
+	pass
+
+
+func _step_27() -> void:
+	pass
+
+
+func _step_28() -> void:
+	pass
+
+
+func _step_29() -> void:
+	pass
+
+
+func _step_30() -> void:
+	pass
+
+
+func _step_31() -> void:
+	pass
+
+
+func _step_32() -> void:
+	pass
+
+
+func _step_33() -> void:
+	pass
+
+
+func _step_34() -> void:
+	pass
+
+
+func _step_35() -> void:
+	pass
+
+
+func _step_36() -> void:
+	pass
+
+
+func _step_37() -> void:
+	pass
+
+
+func _step_38() -> void:
+	pass
+
+
+func _step_39() -> void:
+	pass
+
+
+func _step_40() -> void:
+	pass
+
+
+func _step_41() -> void:
+	pass
+
+
+func _step_42() -> void:
+	pass
+
+
+func _step_43() -> void:
+	pass
+
+
+func _step_44() -> void:
+	pass
+
+
+func _step_45() -> void:
+	pass
+
+
+func _step_46() -> void:
+	pass
+
+
+func _step_47() -> void:
+	pass
+
+
+func _step_48() -> void:
+	pass
+
+
+func _step_49() -> void:
+	pass
+
+
+func _step_50() -> void:
+	pass
+
+
+func _step_51() -> void:
+	pass
+
+
+func _step_52() -> void:
+	pass
+
+
+func _step_53() -> void:
+	pass
+
+
+func _step_54() -> void:
+	pass
+
+
+func _step_55() -> void:
+	pass
+
+
+func _step_56() -> void:
+	pass
+
+
+func _step_57() -> void:
+	pass
+
+
+func _step_58() -> void:
+	pass
+
+
+func _step_59() -> void:
+	pass
+
+
+func _step_60() -> void:
+	pass
+
+
+func _step_61() -> void:
+	pass
+
+
+func _step_62() -> void:
+	pass
+
+
+func _step_63() -> void:
+	pass
+
+
+func _step_64() -> void:
+	pass
+
+
+func _step_65() -> void:
+	pass
+
+
+func _step_66() -> void:
+	pass
+
+
+func _step_67() -> void:
+	pass
+
+
+func _step_68() -> void:
+	pass
+
+
+func _step_69() -> void:
+	pass
+
+
+func _step_70() -> void:
+	pass
+
+
+func _step_71() -> void:
+	pass
+
+
+func _step_72() -> void:
+	pass
+
+
+func _step_73() -> void:
+	pass
+
+
+func _step_74() -> void:
+	pass
+
+
+func _step_75() -> void:
+	pass
+
+
+func _step_76() -> void:
+	pass
+
+
+func _step_77() -> void:
+	pass
+
+
+func _step_78() -> void:
+	pass
+
+
+func _step_79() -> void:
+	pass
+
+
+func _step_80() -> void:
+	pass
+
+
+func _step_81() -> void:
+	pass
+
+
+func _step_82() -> void:
+	pass
+
+
+func _step_83() -> void:
+	pass
+
+
+func _step_84() -> void:
+	pass
+
+
+func _step_85() -> void:
+	pass
+
+
+func _step_86() -> void:
+	pass
+
+
+func _step_87() -> void:
+	pass
+
+
+func _step_88() -> void:
+	pass
+
+
+func _step_89() -> void:
+	pass
+
+
+func _step_90() -> void:
+	pass
+
+
+func _step_91() -> void:
+	pass
+
+
+func _step_92() -> void:
+	pass
+
+
+func _step_93() -> void:
+	pass
+
+
+func _step_94() -> void:
+	pass
+
+
+func _step_95() -> void:
+	pass
+
+
+func _step_96() -> void:
+	pass
+
+
+func _step_97() -> void:
+	pass
+
+
+func _step_98() -> void:
+	pass
+
+
+func _step_99() -> void:
+	pass
+
+
+func _step_100() -> void:
+	pass
+
+
+func _step_101() -> void:
+	pass
+
+
+func _step_102() -> void:
+	pass
+
+
+func _step_103() -> void:
+	pass
+
+
+func _step_104() -> void:
+	pass
+
+
+func _step_105() -> void:
+	pass
+
+
+func _step_106() -> void:
+	pass
+
+
+func _step_107() -> void:
+	pass
+
+
+func _step_108() -> void:
+	pass
+
+
+func _step_109() -> void:
+	pass
+
+
+func _step_110() -> void:
+	pass
+
+
+func _step_111() -> void:
+	pass
+
+
+func _step_112() -> void:
+	pass
+
+
+func _step_113() -> void:
+	pass
+
+
+func _step_114() -> void:
+	pass
+
+
+func _step_115() -> void:
+	pass
+
+
+func _step_116() -> void:
+	pass
+
+
+func _step_117() -> void:
+	pass
+
+
+func _step_118() -> void:
+	pass
+
+
+func _step_119() -> void:
+	pass
+
+
+func _step_120() -> void:
+	pass
+
+
+func _step_121() -> void:
+	pass
+
+
+func _step_122() -> void:
+	pass
+
+
+func _step_123() -> void:
+	pass
+
+
+func _step_124() -> void:
+	pass
+
+
+func _step_125() -> void:
+	pass
+
+
+func _step_126() -> void:
+	pass
+
+
+func _step_127() -> void:
+	pass
+
+
+func _step_128() -> void:
+	pass
+
+
+func _step_129() -> void:
+	pass
+
+
+func _step_130() -> void:
+	pass
+
+
+func _step_131() -> void:
+	pass
+
+
+func _step_132() -> void:
+	pass
+
+
+func _step_133() -> void:
+	pass
+
+
+func _step_134() -> void:
+	pass
+
+
+func _step_135() -> void:
+	pass
+
+
+func _step_136() -> void:
+	pass
+
+
+func _step_137() -> void:
+	pass
+
+
+func _step_138() -> void:
+	pass
+
+
+func _step_139() -> void:
+	pass
+
+
+func _step_140() -> void:
+	pass
+
+
+func _step_141() -> void:
+	pass
+
+
+func _step_142() -> void:
+	pass
+
+
+func _step_143() -> void:
+	pass
+
+
+func _step_144() -> void:
+	pass
+
+
+func _step_145() -> void:
+	pass
+
+
+func _step_146() -> void:
+	pass
+
+
+func _step_147() -> void:
+	pass
+
+
+func _step_148() -> void:
+	pass
+
+
+func _step_149() -> void:
+	pass
+
+
+func _step_150() -> void:
+	pass
+
+
+func _step_151() -> void:
+	pass
+
+
+func _step_152() -> void:
+	pass
+
+
+func _step_153() -> void:
+	pass
+
+
+func _step_154() -> void:
+	pass
+
+
+func _step_155() -> void:
+	pass
+
+
+func _step_156() -> void:
+	pass
+
+
+func _step_157() -> void:
+	pass
+
+
+func _step_158() -> void:
+	pass
+
+
+func _step_159() -> void:
+	pass
+
+
+func _step_160() -> void:
+	pass
+
+
+func _step_161() -> void:
+	pass
+
+
+func _step_162() -> void:
+	pass
+
+
+func _step_163() -> void:
+	pass
+
+
+func _step_164() -> void:
+	pass
+
+
+func _step_165() -> void:
+	pass
+
+
+func _step_166() -> void:
+	pass
+
+
+func _step_167() -> void:
+	pass
+
+
+func _step_168() -> void:
+	pass
+
+
+func _step_169() -> void:
+	pass
+
+
+func _step_170() -> void:
+	pass
+
+
+func _step_171() -> void:
+	pass
+
+
+func _step_172() -> void:
+	pass
+
+
+func _step_173() -> void:
+	pass
+
+
+func _step_174() -> void:
+	pass
+
+
+func _step_175() -> void:
+	pass
+
+
+func _step_176() -> void:
+	pass
+
+
+func _step_177() -> void:
+	pass
+
+
+func _step_178() -> void:
+	pass
+
+
+func _step_179() -> void:
+	pass
+
+
+func _step_180() -> void:
+	pass
+
+
+func _step_181() -> void:
+	pass
+
+
+func _step_182() -> void:
+	pass
+
+
+func _step_183() -> void:
+	pass
+
+
+func _step_184() -> void:
+	pass
+
+
+func _step_185() -> void:
+	pass
+
+
+func _step_186() -> void:
+	pass
+
+
+func _step_187() -> void:
+	pass
+
+
+func _step_188() -> void:
+	pass
+
+
+func _step_189() -> void:
+	pass
+
+
+func _step_190() -> void:
+	pass
+
+
+func _step_191() -> void:
+	pass
+
+
+func _step_192() -> void:
+	pass
+
+
+func _step_193() -> void:
+	pass
+
+
+func _step_194() -> void:
+	pass
+
+
+func _step_195() -> void:
+	pass
+
+
+func _step_196() -> void:
+	pass
+
+
+func _step_197() -> void:
+	pass
+
+
+func _step_198() -> void:
+	pass
+
+
+func _step_199() -> void:
+	pass
+
+
+func _step_200() -> void:
+	pass
+
+
+func _step_201() -> void:
+	pass
+
+
+func _step_202() -> void:
+	pass
+
+
+func _step_203() -> void:
+	pass
+
+
+func _step_204() -> void:
+	pass
+
+
+func _step_205() -> void:
+	pass
+
+
+func _step_206() -> void:
+	pass
+
+
+func _step_207() -> void:
+	pass
+
+
+func _step_208() -> void:
+	pass
+
+
+func _step_209() -> void:
+	pass
+
+
+func _step_210() -> void:
+	pass
+
+
+func _step_211() -> void:
+	pass
+
+
+func _step_212() -> void:
+	pass
+
+
+func _step_213() -> void:
+	pass
+
+
+func _step_214() -> void:
+	pass
+
+
+func _step_215() -> void:
+	pass
+
+
+func _step_216() -> void:
+	pass
+
+
+func _step_217() -> void:
+	pass
+
+
+func _step_218() -> void:
+	pass
+
+
+func _step_219() -> void:
+	pass
+
+
+func _step_220() -> void:
+	pass
+
+
+func _step_221() -> void:
+	pass
+
+
+func _step_222() -> void:
+	pass
+
+
+func _step_223() -> void:
+	pass
+
+
+func _step_224() -> void:
+	pass
+
+
+func _step_225() -> void:
+	pass
+
+
+func _step_226() -> void:
+	pass
+
+
+func _step_227() -> void:
+	pass
+
+
+func _step_228() -> void:
+	pass
+
+
+func _step_229() -> void:
+	pass
+
+
+func _step_230() -> void:
+	pass
+
+
+func _step_231() -> void:
+	pass
+
+
+func _step_232() -> void:
+	pass
+
+
+func _step_233() -> void:
+	pass
+
+
+func _step_234() -> void:
+	pass
+
+
+func _step_235() -> void:
+	pass
+
+
+func _step_236() -> void:
+	pass
+
+
+func _step_237() -> void:
+	pass
+
+
+func _step_238() -> void:
+	pass
+
+
+func _step_239() -> void:
+	pass
+
+
+func _step_240() -> void:
+	pass
+
+
+func _step_241() -> void:
+	pass
+
+
+func _step_242() -> void:
+	pass
+
+
+func _step_243() -> void:
+	pass
+
+
+func _step_244() -> void:
+	pass
+
+
+func _step_245() -> void:
+	pass
+
+
+func _step_246() -> void:
+	pass
+
+
+func _step_247() -> void:
+	pass
+
+
+func _step_248() -> void:
+	pass
+
+
+func _step_249() -> void:
+	pass
+
+
+func _step_250() -> void:
+	pass
+
+
+func _step_251() -> void:
+	pass
+
+
+func _step_252() -> void:
+	pass
+
+
+func _step_253() -> void:
+	pass
+
+
+func _step_254() -> void:
+	pass
+
+
+func _step_255() -> void:
+	pass
+
+
+func _step_256() -> void:
+	pass
+
+
+func _step_257() -> void:
+	pass
+
+
+func _step_258() -> void:
+	pass
+
+
+func _step_259() -> void:
+	pass
+
+
+func _step_260() -> void:
+	pass
+
+
+func _step_261() -> void:
+	pass
+
+
+func _step_262() -> void:
+	pass
+
+
+func _step_263() -> void:
+	pass
+
+
+func _step_264() -> void:
+	pass
+
+
+func _step_265() -> void:
+	pass
+
+
+func _step_266() -> void:
+	pass
+
+
+func _step_267() -> void:
+	pass
+
+
+func _step_268() -> void:
+	pass
+
+
+func _step_269() -> void:
+	pass
+
+
+func _step_270() -> void:
+	pass
+
+
+func _step_271() -> void:
+	pass
+
+
+func _step_272() -> void:
+	pass
+
+
+func _step_273() -> void:
+	pass
+
+
+func _step_274() -> void:
+	pass
+
+
+func _step_275() -> void:
+	pass
+
+
+func _step_276() -> void:
+	pass
+
+
+func _step_277() -> void:
+	pass
+
+
+func _step_278() -> void:
+	pass
+
+
+func _step_279() -> void:
+	pass
+
+
+func _step_280() -> void:
+	pass
+
+
+func _step_281() -> void:
+	pass
+
+
+func _step_282() -> void:
+	pass
+
+
+func _step_283() -> void:
+	pass
+
+
+func _step_284() -> void:
+	pass
+
+
+func _step_285() -> void:
+	pass
+
+
+func _step_286() -> void:
+	pass
+
+
+func _step_287() -> void:
+	pass
+
+
+func _step_288() -> void:
+	pass
+
+
+func _step_289() -> void:
+	pass
+
+
+func _step_290() -> void:
+	pass
+
+
+func _step_291() -> void:
+	pass
+
+
+func _step_292() -> void:
+	pass
+
+
+func _step_293() -> void:
+	pass
+
+
+func _step_294() -> void:
+	pass
+
+
+func _step_295() -> void:
+	pass
+
+
+func _step_296() -> void:
+	pass
+
+
+func _step_297() -> void:
+	pass
+
+
+func _step_298() -> void:
+	pass
+
+
+func _step_299() -> void:
+	pass
+
+
+func _step_300() -> void:
+	pass
+
+
+func _step_301() -> void:
+	pass
+
+
+func _step_302() -> void:
+	pass
+
+
+func _step_303() -> void:
+	pass
+
+
+func _step_304() -> void:
+	pass
+
+
+func _step_305() -> void:
+	pass
+
+
+func _step_306() -> void:
+	pass
+
+
+func _step_307() -> void:
+	pass
+
+
+func _step_308() -> void:
+	pass
+
+
+func _step_309() -> void:
+	pass
+
+
+func _step_310() -> void:
+	pass
+
+
+func _step_311() -> void:
+	pass
+
+
+func _step_312() -> void:
+	pass
+
+
+func _step_313() -> void:
+	pass
+
+
+func _step_314() -> void:
+	pass
+
+
+func _step_315() -> void:
+	pass
+
+
+func _step_316() -> void:
+	pass
+
+
+func _step_317() -> void:
+	pass
+
+
+func _step_318() -> void:
+	pass
+
+
+func _step_319() -> void:
+	pass
+
+
+func _step_320() -> void:
+	pass
+
+
+func _step_321() -> void:
+	pass
+
+
+func _step_322() -> void:
+	pass
+
+
+func _step_323() -> void:
+	pass
+
+
+func _step_324() -> void:
+	pass
+
+
+func _step_325() -> void:
+	pass
+
+
+func _step_326() -> void:
+	pass
+
+
+func _step_327() -> void:
+	pass
+
+
+func _step_328() -> void:
+	pass
+
+
+func _step_329() -> void:
+	pass
+
+
+func _step_330() -> void:
+	pass
+
+
+func _step_331() -> void:
+	pass
+
+
+func _step_332() -> void:
+	pass
+
+
+func _step_333() -> void:
+	pass
+
+
+func _step_334() -> void:
+	pass
+
+
+func _step_335() -> void:
+	pass
+
+
+func _step_336() -> void:
+	pass
+
+
+func _step_337() -> void:
+	pass
+
+
+func _step_338() -> void:
+	pass
+
+
+func _step_339() -> void:
+	pass
+
+
+# a demonstration comment that is deliberately far longer than the hundred character limit gdlint enforces a demonstration comment that is deliberately far longer than the hundred character limit gdlint enforces
+func _step_last() -> void:
+	pass
